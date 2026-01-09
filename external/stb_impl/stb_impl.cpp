b9d7e3938fb5f08c27af7372aaf6a92085a5d399
coolsigma103 <coolsigma1003@gmail.com>
2026-01-09 17:13:16 +0700

    project set up

---
+++ external/stb_impl/stb_impl.cpp
@@ -0,0 +1,25 @@
+// stb_impl.cpp - STB library implementations
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+// #include <stb/stb_image_write.h>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <stb/stb_truetype.h>
+
+#define STB_RECT_PACK_IMPLEMENTATION
+// #include <stb/stb_rect_pack.h>
+
+#define STB_DS_IMPLEMENTATION
+// #include <stb/stb_ds.h>
+
+#define STB_SPRINTF_IMPLEMENTATION
+// #include <stb/stb_sprintf.h>
+
+#define STB_VORBIS_IMPLEMENTATION
+// #include <stb/stb_vorbis.c>
+
+#define STB_TILEMAP_EDITOR_IMPLEMENTATION
+// #include <stb/stb_tilemap_editor.h>
